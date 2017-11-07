7268524137e34c5837be9a1bf980592b3f94d0c2
iacopy <iacopomarmorini@gmail.com>
2017-11-07 09:10:56 +0100

    draw polygons with fill and transparency

--- src/drawer.py
+++ src/drawer.py
@@ -35,7 +35,7 @@ class TrianglesEncoder:
 
     def decode(self, dna):
         """
-        Da una sequenza binaria produce `self.n_triangles` triangoli.
+        Da una sequenza binaria produce poligoni.
         """
         i = 0
         w_bit, h_bit = self.w_bit, self.h_bit
@@ -72,9 +72,85 @@ def draw_triangles(image_size, triangles, background_color=255, save=False):
     return im
 
 
+class PolygonsEncoder:
+    def __init__(self, image_size, n_total_sides, min_sides=3, max_sides=4):
+        width, height = image_size
+        self.width_bits = ceil(log(width, 2))
+        self.height_bits = ceil(log(height, 2))
+        self.image_size = image_size
+        sides_span = max_sides - min_sides + 1
+        self.sides = tuple(range(min_sides, max_sides + 1))
+        self.sides_bits = ceil(log(sides_span, 2))
+        self.color_bits = ceil(log(256, 2))
+        self.colors_channels = 4
+        n_polygons = int(n_total_sides / max_sides)
+        self.genome_size = self.color_bits * self.colors_channels + \
+            n_total_sides * self.width_bits * self.height_bits + \
+            n_polygons * (self.sides_bits + self.color_bits * self.colors_channels)
+        self.index = 0
+
+    def _read(self, sequence, n_bits):
+        chunk = sequence[self.index: self.index + n_bits]
+        dec = int(chunk, 2)
+        #print('chunk = {}, dec = {}'.format(chunk, dec), end=' ')
+        self.index += n_bits
+        return dec
+
+    def _read_points(self, sequence, n_points):
+        points = [(0, 0)] * n_points
+        for i in range(n_points):
+            x = self._read(sequence, self.width_bits)
+            y = self._read(sequence, self.height_bits)
+            points[i] = x, y
+        return points
+
+    def _read_color(self, sequence):
+        """Read and decode an RGB or RGBA color (based on self.color_channels).
+        """
+        return tuple(
+            [self._read(sequence, self.color_bits)
+                for _ in range(self.colors_channels)]
+        )
+
+    def decode(self, sequence):
+        self.index = 0
+        polygons = []
+        #print('Decoding sequence', sequence, len(sequence))
+        n_chunks = 0
+        bg_color = self._read_color(sequence)
+        while self.index < len(sequence):
+            #print(self.index, n_chunks)
+            n_chunks += 1
+            try:
+                n_sides = self.sides[self._read(sequence, self.sides_bits)]
+                #print('n_sides =', n_sides)
+                # read polygon
+                points = self._read_points(sequence, n_sides)
+                #print('points:', points)
+                color = self._read_color(sequence)
+                #print('color:', color)
+            except ValueError as err:
+                #print(err)
+                break
+            else:
+                polygons.append((points, color))
+        return {'background': bg_color, 'polygons': polygons}
+
+    def generate(self):
+        return ''.join([choice(BASES) for _ in range(self.genome_size)])
+
+
+def draw_polygons(image_size, polygons, background_color='white', image_mode='RGB'):
+    im = Image.new(image_mode, image_size, color=background_color)
+    drawer = ImageDraw.Draw(im, 'RGBA')
+    for points, color in polygons:
+        drawer.polygon(points, color)
+    return im
+
+
 class ImageEvaluator:
-    def __init__(self, target_image):
-        im = Image.open(target_image).convert('L')
+    def __init__(self, target_image, mode='RGB'):
+        im = Image.open(target_image).convert(mode)
         dirpath, filename = os.path.split(target_image)
         name, ext = os.path.splitext(filename)
         self.target_dst_dir = os.path.join(dirpath, name)
@@ -82,7 +158,8 @@ class ImageEvaluator:
             os.makedirs(self.target_dst_dir)
         except FileExistsError:
             print('Directory already exists:', self.target_dst_dir)
-        im.save(os.path.join(self.target_dst_dir, name + '_L' + ext))
+        im.save(os.path.join(self.target_dst_dir, name + '_' + mode + ext))
+        self.target_image = im
         self.target_size = im.size
         self.target_data = np.array(im.getdata()) / 255
         self.n_data = len(self.target_data)
--- src/monnalisa.py
+++ src/monnalisa.py
@@ -1,7 +1,7 @@
 from __future__ import division
-from drawer import TrianglesEncoder
+from drawer import PolygonsEncoder
 from drawer import ImageEvaluator
-from drawer import draw_triangles
+from drawer import draw_polygons
 from genome import flip_mutate
 from genome import rand_positions
 from drawer import __file__ as drawer__file__
@@ -17,33 +17,38 @@ SAVE_INTERVAL = STOP / 10
 MIN_SAVE_DT = 10
 
 
-def main(target, n_triangles):
+def main(target, n_polygons, image_mode='RGB'):
     """
     Simplest GA main function.
     """
     min_save_dt = 0.5
+    print('drawing {} with {} polygons'.format(target, n_polygons))
 
     def save_image(image):
         print('Save image...',)
         dst = os.path.join(
             evaluator.target_dst_dir,
             '{name}_i{i}-t{t}-mse{mse:.4f}.png'.format(
-                name=os.path.basename(target), i=iteration, t=n_triangles, mse=father_evaluation
+                name=os.path.basename(target), i=iteration, t=n_polygons, mse=father_evaluation
             )
         )
         image.save(dst)
         print('saved', dst)
 
     print('target: {}'.format(repr(target)))
-    print('n_triangles: {}'.format(repr(n_triangles)))
+    print('n_polygons: {}'.format(n_polygons))
+    n_total_sides = n_polygons * 3
+    print('n_total_sides: {}'.format(n_total_sides))
 
     evaluator = ImageEvaluator(target)
     image_size = evaluator.target_size
-    triangles_encoder = TrianglesEncoder(image_size, n_triangles)
-    genome_size = triangles_encoder.genome_size
-    father = triangles_encoder.generate()
-    father_triangles = triangles_encoder.decode(father)
-    father_phenotype = draw_triangles(image_size, father_triangles)
+    polygons_encoder = PolygonsEncoder(
+        image_size, n_total_sides, min_sides=3, max_sides=4)
+    genome_size = polygons_encoder.genome_size
+    father = polygons_encoder.generate()
+    father_im_recipe = polygons_encoder.decode(father)
+    father_phenotype = draw_polygons(image_size, father_im_recipe['polygons'],
+        background_color=father_im_recipe['background'], image_mode=image_mode)
     father_evaluation = evaluator.evaluate(father_phenotype)
     best_image = father_phenotype
 
@@ -57,8 +62,9 @@ def main(target, n_triangles):
 
         mut_positions = rand_positions(genome_size, 1.0 / genome_size)
         child = flip_mutate(mut_positions, father)
-        child_triangles = triangles_encoder.decode(child)
-        child_phenotype = draw_triangles(image_size, child_triangles)
+        child_im_recipe = polygons_encoder.decode(child)
+        child_phenotype = draw_polygons(image_size, child_im_recipe['polygons'],
+            background_color=child_im_recipe['background'], image_mode=image_mode)
         child_evaluation = evaluator.evaluate(child_phenotype)
         if child_evaluation < father_evaluation:
             print('success at {:,}'.format(iteration))
@@ -93,7 +99,7 @@ def main(target, n_triangles):
         impl_name + version + '-' + os.path.basename(drawer__file__)
     )
     with open('{}.bench.txt'.format(filename), 'a') as fp:
-        fp.write(speed + '\n')
+        fp.write('{}; {}; {}\n'.format(father_phenotype.size, father_phenotype.mode, speed))
     print(drawer__file__)
     print('{:,} iterations in {:.2f} s'.format(iteration, et))
     print(speed)
@@ -102,10 +108,10 @@ def main(target, n_triangles):
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) == 2:
-        target, n_triangles = args
+        target, n_polygons = args
     elif len(args) == 1:
-        target, n_triangles = args[0], N_TRIANGLES
+        target, n_polygons = args[0], N_POLYGONS
     elif len(args) == 0:
-        target, n_triangles = 'images/monnalisa.png', N_TRIANGLES
+        target, n_polygons = 'images/monnalisa.png', N_POLYGONS
 
-    main(target, int(n_triangles))
+    main(target, int(n_polygons))
