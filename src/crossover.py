d2dce9d4a00f5c6c020025b5635f8ee6136ae6df
iacopy <iacopomarmorini@gmail.com>
2017-12-21 20:58:28 +0100

    punti di ricombinazione distribuiti normalmente

--- src/crossover.py
+++ src/crossover.py
@@ -52,20 +52,45 @@ def crossover(seq1, seq2, points=()):
     return ret1, ret2
 
 
-def normal_rand_crossover_operator(seq1, seq2, mu=1, sigma=0.666, min_n_events=1):
+def normal_uniform_rand_crossover_operator(seq1, seq2, mu=1, sigma=0.666, min_n_events=1):
     """
     Restituisce una lista di n (numero casuale con distribuzione normale definita da `mu` e `sigma`)
-    posizioni casuali (ciascuna posizione spazia tra 0 e la lunghezza delle sequenza più lunga).
+    posizioni uniformemente casuali
+    (ciascuna posizione spazia tra 0 e la lunghezza delle sequenza più lunga).
 
     :param seq1: first sequence
     :param seq2: second sequence
-    :param mu: normalvariate mu parameter
-    :param sigma: normalvariate sigma parameter
-    :param min_n_events: minumum number of points (recombination events)
-    :return: list of crossover points
+    :param mu: parametro `mu` per la normalvariate che estrae il numero di punti di c.o.
+    :param sigma: parametro `sigma` per v. sopra
+    :param min_n_events: numero minimo di eventi di ricombinazione
+    :return: lista dei punti di ricombinazione
     """
     n_points = max(int((round(abs(normalvariate(mu, sigma))))), min_n_events)
     return sorted(sample(range(max(map(len, [seq1, seq2]))), n_points))
+
+
+def normal_rand_crossover_operator(
+        seq1, seq2,
+        mu=1, sigma=0.666, min_n_events=1,
+        pos_mu=0.5, pos_sigma=0.125,
+    ):
+    """
+    Restituisce una lista di punti di ricombinazione normalmente distribuiti,
+
+    :param seq1: prima sequenza sorgente
+    :param seq2: seconda sequenza sorgente
+    :param mu: mu per il numero di punti di crossover
+    :param sigma: sigma per il numero di punti di crossover
+    :param min_n_events: minumo numero di eventi di crossover
+    :param pos_mu: mu per le posizioni di crossover (-> mu = pos_mu * lunghezza media seqs)
+    :param pos_sigma: sigma per le posizioni di crossover
+    :retuurn: lista punti di crossover
+    """
+    n_points = max(int((round(abs(normalvariate(mu, sigma))))), min_n_events)
+    span = (len(seq1) + len(seq2)) / 2
+    return [
+        int(round(abs(normalvariate(span * pos_mu, span * pos_sigma)))) for _ in range(n_points)
+    ]
 
 
 def rand_crossover(seq1, seq2, rand_crossover_operator=normal_rand_crossover_operator, **kwargs):
